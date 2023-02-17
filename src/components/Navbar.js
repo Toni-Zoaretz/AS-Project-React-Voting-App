d18fee1b85e0ab6de8c0dc5d854a3112d7313d66
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-17 10:48:12 +0200

    navbar styling only

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <span>Hi User</span>
+      <button className="btn btn-logout">Logout</button>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/pages/Voting.js
+++ src/pages/Voting.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.png";
@@ -7,6 +8,7 @@ import img3 from "../assets/3.png";
 function Voting() {
   return (
     <section className="voting-page">
+      <Navbar />
       <div className="card-contanier">
         <Card img={img1} name={"Name Name1"} totalVote={15} />
         <Card img={img2} name={"Name Name2"} totalVote={10} />
